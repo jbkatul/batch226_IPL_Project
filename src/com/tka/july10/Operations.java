b3b070a8f0fd3d22ea25a794ef51cb5f7b6dc821
pradipshinde12 <shindepradip543@gmail.com>
2025-07-10 21:41:05 +0530

    name added inside method using comment

--- src/com/tka/july10/Operations.java
+++ src/com/tka/july10/Operations.java
@@ -13,6 +13,8 @@ public class Operations {
 	// Method to find max number in an array
 	public int max(int[] a) {
 		
+		//  my self pradip shinde 
+		
 		int max = a[0];
 		for (int i = 1; i < a.length; i++) {
 			if (a[i] > max) {
