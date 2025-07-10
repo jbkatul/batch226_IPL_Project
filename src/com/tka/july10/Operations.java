80c23ca33a151b7dcce60fd540efa57ef8a9b950
abhi bengar <abhibenagr29@gmail.com>
2025-07-10 14:39:30 +0530

    added one new method

--- src/com/tka/july10/Operations.java
+++ src/com/tka/july10/Operations.java
@@ -10,6 +10,15 @@ public class Operations {
 	public void tableof10() {
 		// prem
 	}
+	public void add()
+	{
+		int a=10;
+		int b=20;
+		int result=a+b;
+		System.out.println("sum of two number:"+result);
+		
+		
+	}
 	
 	// add your task here
 
--- src/com/tka/july10/TestOperations.java
+++ src/com/tka/july10/TestOperations.java
@@ -3,6 +3,9 @@ package com.tka.july10;
 public class TestOperations {
 
 	public static void main(String[] args) {
+		Operations  obj=new Operations();
+		obj.add();
+		
 
 //		Test your task here
 		
