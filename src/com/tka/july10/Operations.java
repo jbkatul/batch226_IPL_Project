4098f027c29b5bce8b3692c469298adb9c180311
udpatil2 <udhavdugade2@gmail.com>
2025-07-11 09:33:06 +0530

    Add changes from old udbranch

--- src/com/tka/july10/Operations.java
+++ src/com/tka/july10/Operations.java
@@ -2,6 +2,7 @@ package com.tka.july10;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Scanner;
 
 public class Operations {
 	
@@ -17,14 +18,48 @@ public class Operations {
 }
 	
 	
-	public void tableof10() {
-		// prem
+	public static void tableof10(int no) {
+		System.out.println("Table Of "+no);
+		for(int i=1;i<=10;i++) {
+			System.out.println(no+" x "+i+" = "+(no*i));
+		}
 	}
 	
 	// add your task here
 
 	public static int calculateAge(LocalDate birthDate) {
 	    return Period.between(birthDate, LocalDate.now()).getYears();
+	}
+	public static void Quiz() {
+		
+		Scanner sc = new Scanner(System.in);
+        int score = 0;
+
+        System.out.println("Welcome to the Java Quiz!\n");
+
+        System.out.println("Q1: What is the size of int in Java?");
+        
+        System.out.println("A. 4 bytes\nB. 2 bytes\nC. 8 bytes\nD. 1 byte");
+        System.out.println("Enter Your Option");
+        String ans1 = sc.nextLine();
+        if (ans1.equalsIgnoreCase("A")) 
+        	score++;
+
+        System.out.println("\nQ2: Which keyword is used to create a subclass?");
+        System.out.println("A. class\nB. extends\nC. implements\nD. inherits");
+        System.out.println("Enter Your Option");
+        String ans2 = sc.nextLine();
+        if (ans2.equalsIgnoreCase("B")) score++;
+
+        System.out.println("\nQ3: What does JVM stand for?");
+        System.out.println("A. Java Variable Machine\nB. Java Virtual Method\nC. Java Virtual Machine\nD. Java Verified Machine");
+        System.out.println("Enter Your Option");
+        String ans3 = sc.nextLine();
+        if (ans3.equalsIgnoreCase("C")) score++;
+
+        System.out.println("\nYour final score: " + score + "/3");
+
+        sc.close();
 	}
 
 }
--- src/com/tka/july10/TestOperations.java
+++ src/com/tka/july10/TestOperations.java
@@ -6,15 +6,20 @@ public class TestOperations {
 
 	public static void main(String[] args) {
 
+<<<<<<< HEAD
 		
 		 Operations op=new  Operations();
 		
 		 System.out.println( " addition is  :" +op.add(12, 34));
 //		Test your task here
+=======
+		Operations.tableof10(10);
+		//		Test your task here
+>>>>>>> cfac9c9e490d8f2fb7794d89445354cd5cabde78
 		
 		System.out.println("Shubham's age is : " + Operations.calculateAge(LocalDate.of(2002, 5, 2))); 
 
-		
+		Operations.Quiz();// Task Done By Tukaram
 	}
 
 }
