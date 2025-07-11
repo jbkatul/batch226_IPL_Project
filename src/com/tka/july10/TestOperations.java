496a98d59a3a7570b44d285024f99b889f943309
ankita28sep <ankitakudale4@gmail.com>
2025-07-11 13:07:21 +0530

    Completed vowel counter task and tested it

--- src/com/tka/july10/TestOperations.java
+++ src/com/tka/july10/TestOperations.java
@@ -12,6 +12,12 @@ public class TestOperations {
 		System.out.println("Shubham's age is : " + Operations.calculateAge(LocalDate.of(2002, 5, 2))); 
 
 		Operations.Quiz();// Task Done By Tukaram
+		String str=" The dog jumped over the fence to chase the butterfly.";
+		System.out.println("\nVowels present in particular word : ");
+		int number=Operations.countVowels(str);
+		System.out.println("Number of Vowels in given string are: "+number);
 	}
 
-}
+	}
+
+
